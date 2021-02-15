d7ee3056360bf98b7c022bf89528dd2075a5fb55
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-15 14:42:54 -0500

    Add faker gem and seed data

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -1,2 +1,5 @@
 class ScoresController < ApplicationController
+    def index
+        scores = Score.all
+        render json
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    user = User.create(name: Faker::Name.first_name)
+    user.scores << Score.create(number: rand(1...10))
+end
