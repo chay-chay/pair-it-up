f9fbe033e973721b2b7f97da0312d21ce59f654b
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-18 18:26:15 -0500

    added fetch data for backend

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,7 +1,24 @@
 class Data {
   constructor() {
     this.BASE_URL = "http://localhost:3000";
+    this.submit
   }
+
+  submit() {
+    $("#winModal").modal("show");
+    const submit = document.querySelector('.submit');
+    submit.addEventListener("click", () => this.handleSubmit());
+  }
+
+  handleSubmit(){
+    // console.log(`${this.BASE_URL}/topten`)
+  document.querySelector('#name').value
+  this.getUser();
+  this.getRanks();
+  
+  
+  }
+
   getUser(data) {
     return fetch(`${this.BASE_URL}/users`, {
       method: "POST",
@@ -12,7 +29,7 @@ class Data {
       body: JSON.stringify(data),
     })
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      // .then((response) => console.log(response))
       .catch((error) => {
         console.error("Error:", error);
       });
@@ -20,6 +37,46 @@ class Data {
 
   getRanks() {
     return fetch(`${this.BASE_URL}/topten`)
-    .then((response) => response.json());
+    .then((response) => response.json())
+    .then((data => {console.log(this)
+      this.getScore(data)}));
+  }
+
+  getScore(scores){
+    console.log(scores)
+    scores.forEach(e => {
+      let html = `
+       <tr>
+          <td>${e.rank}</td>
+           <td>${e.username}</td>
+           <td>${e.num}</td>
+       </tr>
+      `
+      $('#rank table tbody').innerHTML += html;
+      
+      document.querySelector('#rank').style.display = 'block'
+      document.querySelector('#deck').style.display = 'block'
+      });
+  
+    // const scoresArray = data
+    // let li = document.createElement('li')
+    // li.innerText = m['']
+
+    
+    // response.map((m) =>{
+    //   let li = document.createElement('li')
+    //   li.innerText = m['name']
+    //   console.log(li)
+    //   return li;
+    //   })
   }
 }
+let data = new Data 
+data.submit() 
+// let submit = document.querySelector('.submit')
+// submit.addEventListener('click', handleSubmit);
+
+// function handleSubmit(e){
+//   document.querySelector('#name').value
+//   debugger
+// }
--- frontend/src/game.js
+++ frontend/src/game.js
@@ -1,151 +1,149 @@
-
 class Game {
-    constructor() {
-        this.cardClickCount = 0;
-        this.seconds = 0;
-        this.timer;
-        this.moves = 0;
-        this.correct = 0;
-        this.wrongMoves = 0;
-        this.ended = false;
-        this.cardManager = new CardManager();
-    }
+  constructor() {
+    this.cardClickCount = 0;
+    this.seconds = 0;
+    this.timer;
+    this.moves = 0;
+    this.correct = 0;
+    this.wrongMoves = 0;
+    this.ended = false;
+    this.cardManager = new CardManager();
+  }
 
-    
+  start() {
+    this.cardManager.generateCards();
+    this.cardManager.shuffleCards();
+    this.cardManager.attachClassToCard();
+    this.startTimer();
+    this.showMoves();
+    this.onRestart();
+    this.onClick();
+  }
 
-    start() {
-        this.cardManager.generateCards();
-        this.cardManager.shuffleCards();
-        this.cardManager.attachClassToCard();
-        this.startTimer();
-        this.showMoves();
-        this.onRestart();
-        this.onClick();
-    }
+  onClick() {
+    const deck = document.querySelector(".deck");
 
-    onClick() {
-        const deck = document.querySelector('.deck');
+    deck.addEventListener("click", (event) => {
+      if (event.target.className.includes("card")) {
+        console.log(event.target.id);
 
-        deck.addEventListener('click', (event) => {
-            if (event.target.className.includes('card')) {
-                console.log(event.target.id)
-               
-                event.target.classList.add('show', 'open');
-                this.cardClickCount++;
+        event.target.classList.add("show", "open");
+        this.cardClickCount++;
 
-                if (this.cardClickCount %2 === 0) {
-                    this.checkMatch(this.previousId, event.target.id);
-                } else {
-                    this.previousId = event.target.id;
-                }
-
-            }
-        });
-    }
-
-   
-
-    checkMatch(prevId, currId) {
-       
-        const choice1Card = document.getElementById(prevId);
-        const choice2Card = document.getElementById(currId);
-        // const choice1Class = document.getElementById(prevId).getElementsByClassName('toggle')
-        // const choice2Class = document.getElementById(currId).getElementsByClassName('toggle')
-        const choice1Class = document.getElementById(prevId).querySelector('img').src
-        const choice2Class = document.getElementById(currId).querySelector('img').src
-       
-        this.trackMoves();  
-      
-        if (choice1Class === choice2Class) {
-            this.correct++;
-            this.showScore();
-            this.showMoves();
-            return;
+        if (this.cardClickCount % 2 === 0) {
+          this.checkMatch(this.previousId, event.target.id);
         } else {
-            this.wrongMoves++;
-            this.showMoves();
-            
-            setTimeout(() => {
-                choice1Card.classList.remove('open', 'show');
-                choice2Card.classList.remove('open', 'show');
-            }, 500);
-
+          this.previousId = event.target.id;
         }
+      }
+    });
+  }
+
+  checkMatch(prevId, currId) {
+    const choice1Card = document.getElementById(prevId);
+    const choice2Card = document.getElementById(currId);
+    // const choice1Class = document.getElementById(prevId).getElementsByClassName('toggle')
+    // const choice2Class = document.getElementById(currId).getElementsByClassName('toggle')
+    const choice1Class = document.getElementById(prevId).querySelector("img")
+      .src;
+    const choice2Class = document.getElementById(currId).querySelector("img")
+      .src;
+
+    this.trackMoves();
+
+    if (choice1Class === choice2Class) {
+      this.correct++;
+      this.showScore();
+      this.showMoves();
+      return;
+    } else {
+      this.wrongMoves++;
+      this.showMoves();
+
+      setTimeout(() => {
+        choice1Card.classList.remove("open", "show");
+        choice2Card.classList.remove("open", "show");
+      }, 500);
     }
+  }
 
-    trackMoves() {
-        this.moves++;
+  trackMoves() {
+    this.moves++;
+  }
+
+  showMoves() {
+    const moves = document.querySelector(".moves");
+
+    moves.innerHTML = this.moves;
+  }
+
+  startTimer(status) {
+    const timer = document.querySelector(".timer");
+    this.timer = setInterval(() => {
+      this.seconds++;
+      timer.innerHTML = this.seconds;
+    }, 1000);
+  }
+
+  onRestart() {
+    const button = document.querySelector(".restart");
+
+    button.addEventListener("click", () => this.restart());
+  }
+
+  restart() {
+    this.seconds = 0;
+    this.moves = 0;
+    this.correct = 0;
+    this.cardClickCount = 0;
+    this.showMoves();
+    this.previousId = null;
+    $("#winModal").modal("hide");
+
+    const cardList = document
+      .querySelector(".deck")
+      .getElementsByTagName("div");
+
+    for (let card of cardList) {
+      card.classList.remove("show", "open");
     }
+  }
 
-    showMoves() {
+  showScore() {
+    if (this.correct === 8) {
+      setTimeout(() => {
+        $("#winModal").modal("show");
+        
+        // const winModal = document.getElementById('winModal')
+        // winModal.style.display = 'flex';
+        const winString = `Yay! You win! You took ${this.moves} moves.`;
 
-        const moves = document.querySelector(".moves");
+        const modalText = document
+          .querySelector(".modal-body")
+          .querySelector("h6");
+        modalText.innerHTML = winString;
 
-        moves.innerHTML = this.moves;
+        const close = document.querySelector('.close')
+        close.addEventListener('click', () => this.close()
+        );
+      }, 500);
     }
-
-    startTimer(status) {
-
-        const timer = document.querySelector(".timer");
-        this.timer = setInterval(() => {
-
-            this.seconds++
-            timer.innerHTML = this.seconds;
-        }, 1000);
-    }
-
-    onRestart() {
-        const button = document.querySelector('.restart');
-
-        button.addEventListener('click', () => this.restart());
-    }
-
-    restart() {
-        this.seconds = 0;
-        this.moves = 0;
-        this.correct = 0;
-        this.cardClickCount = 0;
-        this.showMoves();
-        this.previousId = null;
-        $('#winModal').modal('hide');
-
-        const cardList = document.querySelector('.deck').getElementsByTagName('div');
-
-        for (let card of cardList) {
-            card.classList.remove('show', 'open');
-        }
-    }
-
-    showScore() {
-        if (this.correct === 8) {
-            
-            setTimeout(() => {
-                $('#winModal').modal('show');
-                // const winModal = document.getElementById('winModal')
-                // winModal.style.display = 'flex';
-                const winString = `Yay! You win! You took ${this.moves} moves.`
-
-                const modalText = document.querySelector('.modal-body').querySelector('h6');
-                modalText.innerHTML = winString;
-                // this.promptReplay();
-             
-                
-               
-            }, 500);
-
-        }
-    }
-
-    // promptReplay() {
-    //     const playAgain = document.querySelector('.play-again');
-
-    //     playAgain.addEventListener('click', () => {
-    //         this.restart();
-    //     })
-    // }
-
-
+  }
+  close(){
+      const winModalEnd = document.querySelector('#winModal')
+      winModalEnd.style.display = "none";
+  }
 }
+
+// promptReplay() {
+//     const playAgain = document.querySelector('.play-again'); // change to submit
+
+//     playAgain.addEventListener('click', () => {
+//         this.restart();
+//     })
+// }
+
+// }
 
 const game = new Game();
 game.start();
