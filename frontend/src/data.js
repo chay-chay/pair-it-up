5454d83a582d5a9c2ebb6b648ca7ac1532a06122
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 16:38:12 -0500

    update Heroku & Netlify

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,7 +1,7 @@
 class Data {
   constructor() {
     // this.BASE_URL = "http://localhost:3000";
-    this.BASE_URL = "https://shrouded-basin-97764.herokuapp.com";
+    this.BASE_URL = "https://pair-it-up.netlify.app/";
     this.submit;
     this.restartButton();
   }
